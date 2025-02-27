f55de62754a387e2f6c79946f7b17e772e3dc473
Mathieu <mathieu.crosnier15@outlook.fr>
2025-02-27 15:43:50 +0100

    Correction orthographe

--- src/app/components/ProjectSection.js
+++ src/app/components/ProjectSection.js
@@ -8,98 +8,99 @@ const ProjectSection = () => {
     const projects = [
         {
             id: 7,
-            title: "portfolio",
-            description: "Mon Portfolio mettant en valeurs mes différentes caractéristiques, mes projets, mes informations " +
-                "ainsi que mes compétences .",
+            title: "Portfolio",
+            description: "Mon portfolio mettant en valeur mes différentes caractéristiques, mes projets, mes informations " +
+                "ainsi que mes compétences.",
             fullDescription: "Portfolio réalisé avec NextJS, React et Tailwind. Ce site personnel permet de mettre en avant " +
-                "les différentes compétences que j'ai pu apprendre et permet de lister tous les projets \"important\" auxquels " +
-                "j'ai pu participer. On y retrouve aussi pour cursus scolaire ainsi que mon parcour professionnel.",
-            tags: ["Site","Portfolio", "NextJS", "React", "Tailwind"],
+                "les différentes compétences que j'ai pu acquérir et de lister tous les projets \"importants\" auxquels " +
+                "j'ai participé. On y retrouve aussi mon cursus scolaire ainsi que mon parcours professionnel.",
+            tags: ["Site", "Portfolio", "NextJS", "React", "Tailwind"],
             image: "/projectImages/portfolio.png",
-            teckStack: ["NextJS", "React", "Tailwind", "Html/Css"],
-            date: "25/04/2023",
+            techStack: ["NextJS", "React", "Tailwind", "HTML/CSS"],
+            date: "27/02/2025",
             github: "https://github.com/mat37dev/portfolio-next-js"
         },
         {
             id: 6,
-            title: "Site crm-kodoka",
-            description: "Site listant les compétitions de jeux-vidéos disponibles pour un festival.",
-            fullDescription: "Réalisation d'un crm pour l'entreprise en Html/css/javascript/php avec le framework CodeIgniter. " +
+            title: "Site CRM-Kodoka",
+            description: "Site listant les compétitions de jeux vidéo disponibles pour un festival.",
+            fullDescription: "Réalisation d'un CRM pour l'entreprise en HTML/CSS/JavaScript/PHP avec le framework CodeIgniter. " +
                 "Gestion des utilisateurs, des permissions, des devis/factures/avoirs.",
             tags: ["Site", "PHP", "BTS", "CodeIgniter", "Stage"],
             image: "/projectImages/kodoka.png",
-            teckStack: ["Php", "MySql", "Html/Css", "JavaScript", "CodeIgniter"],
+            techStack: ["PHP", "MySQL", "HTML/CSS", "JavaScript", "CodeIgniter"],
             date: "25/04/2023",
             github: "https://github.com/mat37dev/crm-kodoka"
         },
         {
             id: 5,
             title: "Site Geek4Fun",
-            description: "Site listant les compétitions de jeux-vidéos disponibles pour un festival.",
-            fullDescription: "Site réalisé en cours durant ma deuxième année de BTs SIO. Le site a été réalisé par 3 personnes : " +
-                "Valentin Fortin, Timothée Diot et moi. Chacun avec son module. Mon module étant la gestion des tournois " +
-                "et le système de profil et connexion. Le site permet d'enregistrer des jeux, permettant ainsi de proposer " +
-                "de manière dynamique des tournois ou les utilisateurs peuvent s'inscrire. Le site a été fait en php avec une " +
-                "base de donnée MySql et le Framework CodeIgniter.",
+            description: "Site listant les compétitions de jeux vidéo disponibles pour un festival.",
+            fullDescription: "Site réalisé en cours durant ma deuxième année de BTS SIO. Le site a été réalisé par trois personnes : " +
+                "Valentin Fortin, Timothée Diot et moi. Chacun avait son module. Mon module était la gestion des tournois " +
+                "et le système de profil et de connexion. Le site permet d'enregistrer des jeux, permettant ainsi de proposer " +
+                "de manière dynamique des tournois où les utilisateurs peuvent s'inscrire. Le site a été fait en PHP avec une " +
+                "base de données MySQL et le framework CodeIgniter.",
             tags: ["Site", "PHP", "BTS", "CodeIgniter"],
             image: "/projectImages/geek4Fun.png",
-            teckStack: ["Php", "MySql", "Html/Css", "JavaScript", "CodeIgniter"],
+            techStack: ["PHP", "MySQL", "HTML/CSS", "JavaScript", "CodeIgniter"],
             date: "08/11/2022",
             github: "https://github.com/mat37dev/Geek4Fun"
         },
         {
             id: 4,
             title: "Site de Covoiturage",
-            description: "Site de covoiturage pour étudiant réalisé sans Framework.",
-            fullDescription: "Site de covoiturage réalisé durant les vacances entre ma première année de BTS SIO et ma deuxième " +
-                "année. Ce site a été réalisé avec un camarade de classe (Valentin Fortin) en php pure sans framework. Il possède " +
-                "un système de profile, système pour enregistrer/rejoindre un trajet ainsi qu'une barre de recherche pour " +
-                "trouver des covoitureurs qui nous correspond.",
+            description: "Site de covoiturage pour étudiants réalisé sans framework.",
+            fullDescription: "Site de covoiturage réalisé durant les vacances entre ma première et ma deuxième année de BTS SIO. " +
+                "Ce site a été réalisé avec un camarade de classe (Valentin Fortin) en PHP pur, sans framework. Il possède " +
+                "un système de profil, un système pour enregistrer/rejoindre un trajet ainsi qu'une barre de recherche pour " +
+                "trouver des covoitureurs correspondant à ses besoins.",
             tags: ["Site", "PHP", "BTS", "Covoiturage"],
             image: "/projectImages/covoiturage.png",
-            teckStack: ["Php", "MySql", "Html/Css", "JavaScript"],
+            techStack: ["PHP", "MySQL", "HTML/CSS", "JavaScript"],
             date: "05/05/2020",
         },
         {
             id: 3,
             title: "Find And Build",
-            description: "Création d'un jeu vidéo 3D durant une GameJam",
-            fullDescription: "Jeux vidéo créée durant une GameJam avec deux autres coéquipiers (1 modélisateur, un autre développeur et moi même). " +
-                "Le principe du jeu est de retrouver des morceaux de la ville pour la reconstruire. Règle de la GameJam : " +
+            description: "Création d'un jeu vidéo 3D durant une Game Jam.",
+            fullDescription: "Jeu vidéo créé durant une Game Jam avec deux autres coéquipiers (un modélisateur, un autre développeur et moi-même). " +
+                "Le principe du jeu est de retrouver des morceaux de la ville pour la reconstruire. Règle de la Game Jam : " +
                 "2 jours pour réaliser le jeu sur le thème : \"trouver et construire\".",
-            tags: ["Jeux-Vidéos", "C#", "GameJam"],
+            tags: ["Jeux Vidéo", "C#", "Game Jam"],
             image: "/projectImages/gameJam.png",
-            teckStack: ["C#", "Unity"],
+            techStack: ["C#", "Unity"],
             date: "05/05/2020",
             download: "https://iphinis.itch.io/find-build"
         },
         {
             id: 2,
             title: "Mobile Defender",
-            description: "Création d'un jeu vidéo 3D dans le style Tower Defense",
+            description: "Création d'un jeu vidéo 3D dans le style Tower Defense.",
             fullDescription: "Mobile Defender est un jeu de type Tower Defense sur mobile. L'objectif du jeu était de proposer " +
-                "un jeu en 3D ou le joueur pouvait se balader dans un monde représentant les composants du téléphone. Son objectif, " +
-                "défendre un point vital du téléphone des virus à l'aide de tourelles. Le jeu a été réalisé en C# et était " +
-                "disponible sur le PlayStore.",
-            tags: ["Jeux-Vidéos", "C#"],
+                "un jeu en 3D où le joueur pouvait se balader dans un monde représentant les composants du téléphone. Son objectif : " +
+                "défendre un point vital du téléphone contre des virus à l'aide de tourelles. Le jeu a été réalisé en C# et était " +
+                "disponible sur le Play Store.",
+            tags: ["Jeux Vidéo", "C#"],
             image: "/projectImages/towerDefense.jpg",
-            teckStack: ["C#", "Unity", "Mobile"],
-            date: "Non Définie"
+            techStack: ["C#", "Unity", "Mobile"],
+            date: "Non définie"
         },
         {
             id: 1,
-            title: "Plateformer Runner",
-            description: "Création d'un jeu vidéo 2D dans le style plateformer",
-            fullDescription: "Jeux vidéo mobile créée seul. Le principe du jeu est de réaliser les différents niveaux un peu " +
-                "comme le jeu mario bros. Il  faut se balader, récupérer les pièces, éviter les pièges et ennemis pour finir " +
-                "le niveau. Le jeu comprend un système de vie, de pièce permettant de débloquer des compétences ainsi qu'un " +
-                "petit système de publicité. Le jeu a été réalisé en C# et était disponible sur le PlayStore.",
-            tags: ["Jeux-Vidéos", "C#"],
+            title: "Platformer Runner",
+            description: "Création d'un jeu vidéo 2D dans le style plateforme.",
+            fullDescription: "Jeu vidéo mobile créé seul. Le principe du jeu est de compléter différents niveaux, un peu " +
+                "comme dans Mario Bros. Il faut se déplacer, récupérer des pièces, éviter les pièges et les ennemis pour finir " +
+                "le niveau. Le jeu comprend un système de vies, des pièces permettant de débloquer des compétences ainsi qu'un " +
+                "petit système de publicité. Le jeu a été réalisé en C# et était disponible sur le Play Store.",
+            tags: ["Jeux Vidéo", "C#"],
             image: "/projectImages/plateformer.webp",
-            teckStack: ["C#", "Unity", "Mobile"],
-            date: "Non Définie"
+            techStack: ["C#", "Unity", "Mobile"],
+            date: "Non définie"
         },
     ];
+
 
     const [selectedProject, setSelectedProject] = useState(null);
 
